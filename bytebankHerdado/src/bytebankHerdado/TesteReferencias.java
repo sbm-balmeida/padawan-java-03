93e9d623f542b1fc27c1580ab364adcc6a1841cb
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-07-04 09:50:35 -0300

    Resumo herança

--- bytebankHerdado/src/bytebankHerdado/TesteReferencias.java
+++ bytebankHerdado/src/bytebankHerdado/TesteReferencias.java
@@ -18,11 +18,15 @@ public class TesteReferencias {
 		EditorVideo ev1 = new EditorVideo();
 		ev1.setSalario(2500.0);
 		
+		Designer d = new Designer();
+		d.setSalario(2500.0);
+		
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(g1);
 		controle.registra(f1);
 		controle.registra(ev1);
-		
+		controle.registra(d);
+
 		System.out.println(controle.getSoma());
 	}
 
