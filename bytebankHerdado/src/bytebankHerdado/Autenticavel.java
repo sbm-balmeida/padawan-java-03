5538e1412d2d17e9a4491edfaac896b369eb8fda
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-07-05 10:23:28 -0300

    A primeira interface

--- bytebankHerdado/src/bytebankHerdado/Autenticavel.java
+++ bytebankHerdado/src/bytebankHerdado/Autenticavel.java
@@ -1,19 +1,12 @@
 package bytebankHerdado;
 
-public abstract class Autenticavel {
+//contrato Autenticavel
+	//quem assina esse contrato, precisa implementar
+		//metodo setSenha
+		//metodo autentica
+public abstract interface Autenticavel {
 
-	private int senha;
-	
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
-	
-	public boolean autentica(int senha) {
-		if (this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
+	public abstract void setSenha(int senha);
+		
+	public abstract boolean autentica(int senha); 
 }
